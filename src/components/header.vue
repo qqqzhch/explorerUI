f18e8d1ac6fc97dd44a3cf13d880a65ff1ef3b92
zhangchao <544262408@qq.com>
2018-11-01 14:40:11 +0800

    标签修改

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,22 +3,22 @@
                 <Menu mode="horizontal" theme="dark" active-name="1">
                     <div class="layout-logo"></div>
                     <div class="layout-nav">
-                        <MenuItem name="1">
+                        <Menu-item name="1">
                             <Icon type="ios-navigate"></Icon>
                             Item 1
-                        </MenuItem>
-                        <MenuItem name="2">
+                        </Menu-item>
+                        <Menu-item name="2">
                             <Icon type="ios-keypad"></Icon>
                             Item 2
-                        </MenuItem>
-                        <MenuItem name="3">
+                        </Menu-item>
+                        <Menu-item name="3">
                             <Icon type="ios-analytics"></Icon>
                             Item 3
-                        </MenuItem>
-                        <MenuItem name="4">
+                        </Menu-item>
+                        <Menu-item name="4">
                             <Icon type="ios-paper"></Icon>
                             Item 4
-                        </MenuItem>
+                        </Menu-item>
                     </div>
                 </Menu>
             </Header>
